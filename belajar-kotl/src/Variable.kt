a7b6ad246dc4c988c6031e8746fea52c199020c3
Naufal72k <naufal72k@gmail.com>
2026-03-09 10:32:12 +0800

    belajar

---
+++ belajar-kotl/src/Variable.kt
@@ -0,0 +1,16 @@
+fun namaOrang(a: String) {
+    println("hai $a")
+}
+
+fun main(){
+    val nama: String = "nopal"
+    val umur:Int = 21
+    val alamat: String = "jln sunan giri 4 g 20"
+
+    println("nama saya adalah: $nama")
+    println("umur saya sekarang: $umur")
+    println("dan saya tinggal di: $alamat")
+
+    namaOrang("alya")
+
+}
